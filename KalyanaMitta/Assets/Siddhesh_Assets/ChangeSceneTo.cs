3c71307674fa7a797216289c9d0fe0b3694f1dc0
siddheshgupte <siddhesh.gupte2468@gmail.com>
2018-10-28 08:56:14 +0530

    Add menu and cleanup scenes

---
+++ KalyanaMitta/Assets/Siddhesh_Assets/ChangeSceneTo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// Use this script to change asynchronously to any scene given as a string name.
+public class ChangeSceneTo : MonoBehaviour {
+
+    public string sceneToSwitchTo;
+
+	public void ChangeTo()
+    {
+        StartCoroutine(LoadSceneAsync());
+    }
+
+    IEnumerator LoadSceneAsync()
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneToSwitchTo);
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+}
